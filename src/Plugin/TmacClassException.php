ed704d701d65a9dbb49bfd658cf3ec3f7d10785e
wentmac <zwt007@gmail.com>
2020-05-15 02:17:29 +0800

    rename file

---
+++ src/Plugin/TmacClassException.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Power By Tmac PHP MVC framework
+ * $Author: zhangwentao $  <zwttmac@qq.com>
+ * $Id: TmacClassException.class.php 325 2016-05-31 10:07:35Z zhangwentao $
+ * http://www.t-mac.org； 
+ */
+namespace Tmac\Plugin;
+class TmacClassException extends \Exception
+{
+
+
+    /**
+     * 构造器
+     *
+     * @param string $message
+     * @param int $code
+     * @access public
+     */
+    public function __construct($message = 'Unknown Error', $code = 0)
+    {
+        parent::__construct($message, $code);
+
+    }
+
+}
+
+?>
