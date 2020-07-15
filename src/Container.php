1979e81e3135bf699e5b830dc1b3195e2fa938c2
zhangwentao <zhangwentao@icsoc.net>
2020-07-15 19:04:26 +0800

    容器v1.1增加 动态 * 支持 普通类注册时绑定别名 * 支持  取真实的绑定名称 Object或Closure 对象或闭包回调 * alias（别名可以用来自动注入时，绑定个性化配置的类）如下 * public function __construct( ConfigInterface $tmac_config )

--- src/Container.php
+++ src/Container.php
@@ -73,9 +73,11 @@ class Container implements ArrayAccess, ContainerInterface
      */
     private function setInstance( string $abstract, $instance )
     {
-        $this->_instances[ $abstract ] = $instance;
+        //未设置过的重新设置，container中保存第一次声明初始化的class
+        isset( $this->_instances[ $abstract ] ) || $this->_instances[ $abstract ] = $instance;
         return $this;
     }
+
 
     /**
      * 根据别名获取真实类名
@@ -91,12 +93,13 @@ class Container implements ArrayAccess, ContainerInterface
                 return $this->getAlias( $bind );
             }
             if ( is_array( $bind ) && isset( $bind[ 'class' ] ) ) {
-                //解决了在_instance中保存的都是真实类名当key值
-                $abstract = $bind[ 'class' ];
+                //返回真实的绑定key。alias class name或者原始set的容器key名
+                $abstract = $this->getBindName( $abstract, $bind );
             }
         }
         return $abstract;
     }
+
 
     /**
      * 绑定一个类、闭包、实例、接口实现到容器
@@ -121,6 +124,33 @@ class Container implements ArrayAccess, ContainerInterface
             $this->setBindShared( $abstract, $shared_status );
             return $this;
         }
+
+        /**
+          处理 设置$concrete为Array中有'class'并且设置class为Closure 匿名函数回调
+          ====================================================================
+            $container->setShared( 'fileLogger', [
+               'class' => function ( App $app ) {
+                   $log = new Logger( 'redis' );
+                   $log->pushHandler( new StreamHandler( $app->getVarPath() . 'log/monolog/file_error.log', Logger::WARNING ) );
+                   return $log;
+               },
+               'alias' => \Psr\Log\LoggerInterface::class
+            ] );
+          ==================================================================
+
+         */
+        if ( is_array( $concrete ) && isset( $concrete[ 'class' ] ) && $concrete[ 'class' ] instanceof Closure ) {
+            $this->_bind[ $abstract ] = $concrete[ 'class' ];
+            $this->setBindShared( $abstract, $shared_status );
+
+            //如果设置了alias比如设置了接口类当别名，就绑定在别名上，使用（ConfigInterface $config）之类就能自动解析
+            $bind_name = $this->getObjectBindName( $abstract, $concrete );
+            if ( $bind_name !== false ) {
+                $this->_bind[ $bind_name ] = $abstract;
+            }
+            return $this;
+        }
+
         /**
          * 如果设置的是一个对象，每次调用都会返回实例化
          * 直接注册一个实例
@@ -156,6 +186,32 @@ class Container implements ArrayAccess, ContainerInterface
             $this->setInstance( $abstract, $concrete );
             return $this;
         }
+
+        /**
+          处理 设置$concrete为Array中有'class'并且设置class为Ojbect
+          ============================================================
+
+        $container->setShared( 'boat_c', [
+             'class' => new \App\Service\BoatService('abc'),
+             'alias' => \App\Service\BoatService::class,
+        ] );
+
+          ============================================================
+
+         */
+        if ( is_array( $concrete ) && isset( $concrete[ 'class' ] ) && is_object( $concrete[ 'class' ] ) ) {
+            $this->_bind[ $abstract ] = $concrete[ 'class' ];
+            $this->setBindShared( $abstract, $shared_status );
+            $this->setInstance( $abstract, $concrete[ 'class' ] );
+
+            //如果设置了alias比如设置了接口类当别名，就绑定在别名上，使用（ConfigInterface $config）之类就能自动解析
+            $bind_name = $this->getObjectBindName( $abstract, $concrete );
+            if ( $bind_name !== false ) {
+                $this->_bind[ $bind_name ] = $abstract;
+            }
+            return $this;
+        }
+
         //如果没有绑定类，默认把标识名当类
         if ( $concrete === null ) {
             $concrete = $abstract;
@@ -190,20 +246,125 @@ class Container implements ArrayAccess, ContainerInterface
         }
         if ( is_array( $concrete ) ) {
             //如果是数组，取真实绑定名
-            $this->setBindShared( $concrete[ 'class' ], $shared_status );
-            $this->_bind[ $concrete[ 'class' ] ] = $concrete;
+            //如果设置了alias比如设置了接口类当别名，就绑定在别名上，使用（ConfigInterface $config）之类就能自动解析
+            /**
+             alias（别名可以用来自动注入时，绑定个性化配置的类）如下
+             public function __construct( ConfigInterface $tmac_config, Filter $filter )
+             =========================================
+             $container->setShared( 'tmac_config', [
+                 'class' => Config::class,
+                 'alias' => ConfigInterface::class,
+                 'params' => [
+                     $root_path,
+                     APP_NAME,
+                     '.php'
+                  ]
+             ] );
+             =========================================
+             */
+            $bind_name = $this->getBindName( $abstract, $concrete );
+            //$bind_name = $concrete[ 'class' ];
+            $this->setBindShared( $bind_name, $shared_status );
+
+            if ( empty( $concrete[ 'alias' ] ) ) {
+                //如果绑定时 没有设置 类别名 alias class 直接绑定注入内容到set的key值中a中 set('a',$concrete);
+                $this->_bind[ $abstract ] = $concrete;
+            } else {
+                //如果绑定时 设置的有 类别名 alias class .
+                //_bind[]中 [config] => Tmac\Contract\ConfigInterface $config
+                $this->_bind[ $abstract ] = $bind_name;
+
+                /*
+
+                [Tmac\Contract\ConfigInterface $config] => Array
+                (
+                    [class] => Tmac\Config
+                    [alias] => Tmac\Contract\ConfigInterface
+                    [params] => Array
+                        (
+                            [0] => /var/www/personal/shanhaijingclub/shanhaijing
+                            [1] => admin
+                            [2] => .php
+                        )
+
+                    [class_alias] => Tmac\Contract\ConfigInterface $config
+                )
+                 */
+                $this->_bind[ $bind_name ] = $concrete;
+                $this->_bind[ $bind_name ][ 'class_alias' ] = $bind_name;
+            }
         } else {
             //var_dump($abstract.'='.$concrete.'='.$shared_status);
             //字符串类型的服务别名绑定 比如 $container->setShared('config',Tmac\Config::class);
             //绑定服务别名的服务共享状态 exemple:'config','request'
-            $this->setBindShared( $abstract, $shared_status );
+            /**
+             * 这里不需要set container _bind_shared[] config=1 因为在容器中取类的时候，有getAlias方法查询真实instance名
+             * $this->setBindShared( $abstract, $shared_status );
+             */
             //绑定服务真实类名 example:Tmac\Config::class Tmac\Request::class
             $this->setBindShared( $concrete, $shared_status );
+            $this->_bind[ $abstract ] = $concrete;
         }
-        $this->_bind[ $abstract ] = $concrete;
         return $this;
     }
 
+
+    /**
+     * 取真实的绑定名称
+     * alias（别名可以用来自动注入时，绑定个性化配置的类）如下
+     * ==================================================
+     * public function __construct( ConfigInterface $tmac_config )
+     * ==================================================
+     * @param $abstract
+     * @param $concrete
+     * @return string
+     */
+    private function getBindName( $abstract, $concrete )
+    {
+        //class_alias
+        if ( !empty( $concrete[ 'class_alias' ] ) ) {
+            $bind_name = $concrete[ 'class_alias' ];
+        } else if ( !empty( $concrete[ 'alias' ] ) ) {
+            $bind_name = $concrete[ 'alias' ] . ' $' . $abstract;
+        } else {
+            // $bind_name = $concrete[ 'class' ];
+            /**
+             比如这种情况，没有 设置 alias绑定别名。此时在_bind _bind_shared _instances中绑定的key就返回boat_b
+                $container->setShared( 'boat_b', [
+                    'class' => \App\Service\BoatService::class,
+                    'params' => [
+                        'double duck'
+                    ]
+                ] );
+             */
+            $bind_name = $abstract;
+        }
+        return $bind_name;
+    }
+
+    /**
+     * 取真实的绑定名称 Object或Closure 对象或闭包回调
+     * alias（别名可以用来自动注入时，绑定个性化配置的类）如下
+     * ==================================================
+     * public function __construct( ConfigInterface $tmac_config )
+     * ==================================================
+     * @param $abstract
+     * @param $concrete
+     * @return bool|string
+     */
+    private function getObjectBindName( $abstract, $concrete )
+    {
+        //class_alias
+        if ( !empty( $concrete[ 'class_alias' ] ) ) {
+            $bind_name = $concrete[ 'class_alias' ];
+        } else if ( !empty( $concrete[ 'alias' ] ) ) {
+            $bind_name = $concrete[ 'alias' ] . ' $' . $abstract;
+        } else {
+            //Object或Closure 对象或闭包回调 没有设置 类别名 alias
+            return false;
+        }
+        return $bind_name;
+    }
 
     /**
      * 设置绑定的共享状态
@@ -234,6 +395,7 @@ class Container implements ArrayAccess, ContainerInterface
         return false;
     }
 
+
     /**
      * 检测是否设置了共享服务
      * @param $abstract
@@ -259,6 +421,7 @@ class Container implements ArrayAccess, ContainerInterface
         return false;
     }
 
+
     /**
      * 绑定一个类、闭包、实例、接口实现到容器
      * @access public
@@ -271,6 +434,7 @@ class Container implements ArrayAccess, ContainerInterface
         //设置 注册的类 为非共享服务，每次调用初始化
         return $this->bind( $abstract, $concrete, false );
     }
+
 
     /**
      * 服务可以注册成“shared”类型的服务，这意味着这个服务将使用 [单例模式] 运行，
@@ -298,6 +462,7 @@ class Container implements ArrayAccess, ContainerInterface
         return $this->bind( $abstract, $concrete, true );
     }
 
+
     /**
      * 获取目标实例
      *
@@ -324,6 +489,7 @@ class Container implements ArrayAccess, ContainerInterface
             $this->bind( $abstract, null, $abstract_shared_status );
         }
         $check_bind_shared = $this->isShared( $abstract );
+
         //如果有共享返回共享服务
         if ( $check_bind_shared && isset( $this->_instances[ $abstract ] ) ) {
             $object = $this->_instances[ $abstract ];
@@ -348,6 +514,9 @@ class Container implements ArrayAccess, ContainerInterface
         if ( is_array( $definition ) ) {
             //绑定的抽象类是数组型，从数组中返回要实例化的class_name
             $abstract_name = $definition[ 'class' ];
+            //如果有 class alias 类别名的，下面绑定类别名到 container的_instance[]
+            $bind_name = $this->getBindName( $abstract, $definition );
+
             if ( !empty( $definition[ 'params' ] ) && empty( $params ) ) {
                 $params = $definition[ 'params' ];//构造函数参数注入
             }
@@ -355,15 +524,18 @@ class Container implements ArrayAccess, ContainerInterface
                 $properties = $definition[ 'properties' ];//通过属性注入的服务
             }
         } else {
+            //直接绑定的 #字符串类型的服务别名绑定 比如 $container->setShared('config',Tmac\Config::class);
             $abstract_name = $definition;
+            $bind_name = $definition;
         }
         //未设置过共享服务状态，默认不使用共享
         $object = $this->resolveClass( $abstract_name, $params );
-        $this->setInstance( $abstract_name, $object );
+        $this->setInstance( $bind_name, $object );
         //setter 注入
         $this->bindMethod( $object, $properties );
         return $object;
     }
+
 
     /**
      * 执行函数或者闭包方法 支持参数调用
@@ -383,6 +555,7 @@ class Container implements ArrayAccess, ContainerInterface
         $args = $this->bindParams( $reflect, $params );
         return $function( ...$args );
     }
+
 
     /**
      * 取共享服务，返回单例，无需初始化
@@ -435,6 +608,7 @@ class Container implements ArrayAccess, ContainerInterface
         return $object;
     }
 
+
     /**
      * 自动 注入DI窗口
      * @param ReflectionClass $reflect
@@ -457,7 +631,9 @@ class Container implements ArrayAccess, ContainerInterface
         $method->invokeArgs( $object, $args );
         //$method->invoke( $object );
 
+
     }
+
 
     /**
      * 绑定参数
@@ -504,6 +680,7 @@ class Container implements ArrayAccess, ContainerInterface
         return $args;
     }
 
+
     /**
      * 获取对象类型的参数值
      * @param ReflectionParameter $parameter
@@ -512,13 +689,52 @@ class Container implements ArrayAccess, ContainerInterface
      * @return mixed|object
      * @throws Exception
      */
-
     private function getObjectParam( ReflectionParameter $parameter, bool $associative, array &$vars )
     {
         $class = $parameter->getClass();
         $name = $parameter->getName();
 
         $className = $class->getName();
+
+        /*
+        add start @20200715 支持
+        ===========================================
+        $container->setShared( 'boat_a', [
+            'class' => \App\Service\BoatService::class,
+            'alias' => \App\Service\BoatService::class,
+            'params' => [
+                'fuck1111'
+            ]
+        ] );
+        ===========================================
+        public function indexAction( BoatService $boat_a )
+        ===========================================
+        $container->setShared( 'redisLogger', [
+            'class' => Logger::class,
+            'alias' => \Psr\Log\LoggerInterface::class,
+            'params' => [
+                'redis',
+                [ new StreamHandler( $container->get( 'app' )->getVarPath() . 'log/monolog/redis_error.log', Logger::WARNING ) ],
+                [],
+                new DateTimeZone( date_default_timezone_get() ? : 'UTC' )
+            ]
+        ] );
+        ===========================================
+        public function indexAction( LoggerInterface $redisLogger )
+        ===========================================
+        自动注入的解析
+        */
+        $customize_class_name = $parameter->getClass()->getName() . ' $' . $name;
+        if ( $parameter->getClass() != null
+            && ( $parameter->getClass()->isInterface() || isset( $this->_bind[ $customize_class_name ] ) ) ) {
+            /**
+             * 绑定到接口实现。使用接口作为依赖注入的类型
+             * public function hello(LoggerInterface $log){}
+             */
+            $className = $customize_class_name;
+        }
+        //add end @20200715
+
         if ( $associative && isset( $vars[ $name ] ) && $vars[ $name ] instanceof $className ) {
             //如果是关联数组
             $result = $vars[ $name ];
@@ -530,6 +746,7 @@ class Container implements ArrayAccess, ContainerInterface
             // 重新调用get() 方法获取需要依赖的类到容器中。使用getShared方法。共享自动创建的构造函数注入的服务
             $result = $this->getShared( $className );
         }
+
 
         return $result;
     }
@@ -572,6 +789,7 @@ class Container implements ArrayAccess, ContainerInterface
         return true;
     }
 
+
     /**
      * 判断容器中是否存在类及标识
      *  {@inheritdoc}
@@ -583,6 +801,7 @@ class Container implements ArrayAccess, ContainerInterface
     {
         return isset( $this->_instances[ $id ] );
     }
+
 
     /**
      * 删除容器中的对象实例
@@ -597,6 +816,7 @@ class Container implements ArrayAccess, ContainerInterface
         }
     }
 
+
     /**
      * 判断容器中是否存在对象实例
      * @param string $abstract
@@ -606,6 +826,7 @@ class Container implements ArrayAccess, ContainerInterface
     {
         return isset( $this->_instances[ $abstract ] );
     }
+
 
     /**
      * 通过魔术方法的方式获取
@@ -639,46 +860,55 @@ class Container implements ArrayAccess, ContainerInterface
             return null;
         }
 
+
         // 注意: $name 的值区分大小写
         $error = "Calling object method '$name' "
             . implode( ', ', $arguments ) . "\n";
         throw new ClassNotFoundException( $error );
     }
 
+
     public function __set( $name, $value )
     {
         $this->set( $name, $value );
     }
+
 
     public function __get( $name )
     {
         return $this->get( $name );
     }
 
+
     public function __isset( $name ): bool
     {
         return $this->exists( $name );
     }
+
 
     public function __unset( $name )
     {
         $this->delete( $name );
     }
 
+
     public function offsetExists( $key )
     {
         return $this->exists( $key );
     }
+
 
     public function offsetGet( $key )
     {
         return $this->get( $key );
     }
 
+
     public function offsetSet( $key, $value )
     {
         $this->set( $key, $value );
     }
+
 
     public function offsetUnset( $key )
     {
