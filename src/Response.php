0bd5b4af1c7d5e053f0da6d1b2a17fd921fa6bfc
zhangwentao <zhangwentao@icsoc.net>
2020-07-24 18:49:19 +0800

    框架模板加载类优化修改

--- src/Response.php
+++ src/Response.php
@@ -9,14 +9,25 @@
 
 namespace Tmac;
 
+use Tmac\Contract\ConfigInterface;
+
 class Response
 {
     use DITrait;
 
     protected $tVar = []; // 模板输出变量
-    
-    public function __construct()
+    protected $app;
+    protected $config;
+    protected $request;
+
+    //自定义模板风格
+    protected $template_style = '';
+
+    public function __construct( ConfigInterface $config, App $app, Request $request )
     {
+        $this->config = $config;
+        $this->app = $app;
+        $this->request = $request;
     }
 
     /**
@@ -38,6 +49,22 @@ class Response
     }
 
     /**
+     * 返回模板变量数组
+     * @return array
+     */
+    private function getTplVarArray()
+    {
+        return $array = [
+            'action' => $this->request->getQuery( 'TMAC_ACTION' ),
+            'APP_HOST_URL' => $this->config[ 'app.app_host' ],
+            'APP_PHP_SELF' => basename( $this->request->getServer( 'SCRIPT_NAME' ) ),
+            'STATIC_URL' => $this->config[ 'app.static_url' ],
+            'STATIC_COMMON_URL' => $this->config[ 'app.static_url' ] . 'common/',
+            'STATIC_APP_URL' => $this->config[ 'app.static_url' ] . APP_NAME . '/' . $this->config[ 'app.template.template_dir' ] . '/',
+        ];
+    }
+
+    /**
      * 显示前台模板
      * @param string $tpl 模板文件名 为空时是 CONTROLLER_ACTION
      * @access public
@@ -46,16 +73,8 @@ class Response
     public final function V( $tpl = null )
     {
         //设置模板中的全局变量|前台模板目录
-        $array = array(
-            'action' => $this->container->request->getQuery( 'TMAC_ACTION' ),
-            'APP_HOST_URL' => $this->container->config[ 'app.app_host' ],
-            'APP_PHP_SELF' => basename( $this->container->request->getServer( 'SCRIPT_NAME' ) ),
-            'STATIC_URL' => $this->container->config[ 'app.static_url' ],
-            'STATIC_COMMON_URL' => $this->container->config[ 'app.static_url' ] . 'common/',
-            'STATIC_APP_URL' => $this->container->config[ 'app.static_url' ] . APP_NAME . '/' . $this->container->config[ 'app.template.template_dir' ] . '/',
-        );
-        $this->assign( $array );
-        $tpl = $this->container->config[ 'app.template.template_dir' ] . DIRECTORY_SEPARATOR . $tpl;
+        $this->assign( $this->getTplVarArray() );
+        //$this->template_style = 'blue';//自定义指定加载blue风格模板
         return $this->view( $tpl, $this->tVar );
     }
 
@@ -67,26 +86,31 @@ class Response
     protected function view( string $view = null, array $tVar = null )
     {
         $template_app_name = empty( APP_NAME ) ? 'index' : APP_NAME;
+
+        $template = $this->app->getWebTemplatePath() . $this->config[ 'app.template.template' ] . DIRECTORY_SEPARATOR;
+        $template_style = empty( $this->template_style ) ? $this->config[ 'app.template.template_style' ] : $this->template_style;
         $options = array(
-            'template_dir' => $this->container->app->getWebTemplatePath() . $this->container->config[ 'app.template.template' ], //设置系统模板文件的存放目录
-            'cache_dir' => $this->container->app->getVarPath() . $this->container->config[ 'app.template.cache_dir' ] . DIRECTORY_SEPARATOR . $template_app_name . DIRECTORY_SEPARATOR, //指定缓存文件存放目录
-            'auto_update' => $this->container->config[ 'app.template.auto_update' ], //当模板文件有改动时重新生成缓存 [关闭该项会快一些]
-            'cache_lifetime' => $this->container->config[ 'app.template.cache_lifetime' ], //缓存生命周期(分钟)，为 0 表示永久 [设置为 0 会快一些]
-            'suffix' => $this->container->config[ 'app.template.suffix' ], //模板后缀
-            'cache_open' => $this->container->config[ 'app.template.cache_open' ], //是否开启缓存，程序调试时使用
+            'template' => $template, //设置系统模板文件的存放目录
+            'template_style' => $template_style,
+            'template_dir' => $template . $template_style . DIRECTORY_SEPARATOR,
+            'cache_dir' => $this->app->getVarPath() . $this->config[ 'app.template.cache_dir' ] . DIRECTORY_SEPARATOR . $template_app_name . DIRECTORY_SEPARATOR . $template_style . DIRECTORY_SEPARATOR, //指定缓存文件存放目录
+            'auto_update' => $this->config[ 'app.template.auto_update' ], //当模板文件有改动时重新生成缓存 [关闭该项会快一些]
+            'cache_lifetime' => $this->config[ 'app.template.cache_lifetime' ], //缓存生命周期(分钟)，为 0 表示永久 [设置为 0 会快一些]
+            'suffix' => $this->config[ 'app.template.suffix' ], //模板后缀
+            'cache_open' => $this->config[ 'app.template.cache_open' ], //是否开启缓存，程序调试时使用
             'value' => $tVar    //压到模板里的变量数据
         );
-        $tmac_template_update_cache = $this->container->request->getQuery( 'tmac_template_update_cache', 0 );
+        $tmac_template_update_cache = $this->request->getQuery( 'tmac_template_update_cache', 0 );
         if ( $tmac_template_update_cache ) {
             $options[ 'auto_update' ] = true;//当模板文件有改动时重新生成缓存（适用于关闭主动更新时用于手动更新模板缓存）
         }
         $tpl = $this->container->template;
         $tpl->setOptions( $options ); //设置模板参数
         //如果是前台的模板就不用前缀（模板目录名）
-        if ( $view == $this->container->config[ 'app.template.template_dir' ] . DIRECTORY_SEPARATOR ) {
+        if ( empty( $view ) ) {
             //如果模板路径，文件名为空的话就尝试把TMAC_CONTROLLER_FILE当作模板路径，文件名
-            $view_new = strtolower( $this->container->request->getQuery( 'TMAC_CONTROLLER_FILE' ) . $this->container->request->getQuery( 'TMAC_CONTROLLER_NAME' ) );  //都转成小写的
-            $view = $view . $view_new;  //前面加上模板目录名
+            $view_new = strtolower( $this->request->getQuery( 'TMAC_CONTROLLER_NAME' ) );  //都转成小写的
+            $view = $view_new;  //前面加上模板目录名
         }
         $tpl->show( $view );
     }
@@ -102,12 +126,7 @@ class Response
     public function view_php( $view )
     {
         //设置模板中的全局变量|前台模板目录
-        $array = array(
-            'STATIC_URL' => $this->container->config[ 'app.template.static_url' ],
-            'STATIC_COMMON_URL' => $this->container->config[ 'app.template.static_url' ] . 'common/',
-            'STATIC_APP_URL' => $this->container->config[ 'app.template.static_url' ] . $this->container->config[ 'app.template.template_dir' ] . '/',
-        );
-        $this->assign( $array );
+        $this->assign( $this->getTplVarArray() );
         $this->display( $view, $this->tVar );
     }
 
@@ -118,10 +137,10 @@ class Response
      */
     public final function display( $view, $tVar = null )
     {
-        $file = $this->container->app->getWebTemplatePath() . $this->container->config[ 'app.template.template' ]
-            . DIRECTORY_SEPARATOR . $this->container->config[ 'app.template.template_dir' ]
+        $file = $this->app->getWebTemplatePath() . $this->config[ 'app.template.template' ]
+            . DIRECTORY_SEPARATOR . $this->config[ 'app.template.template_style' ]
             . DIRECTORY_SEPARATOR . $view
-            . $this->container->config[ 'app.template.suffix' ];
+            . $this->config[ 'app.template.suffix' ];
         if ( !empty ( $tVar ) ) {
             extract( $tVar, EXTR_OVERWRITE );
         }
@@ -135,10 +154,10 @@ class Response
      */
     public final function loadView( $view )
     {
-        $file = $this->container->app->getWebTemplatePath() . $this->container->config[ 'app.template.template' ]
-            . DIRECTORY_SEPARATOR . $this->container->config[ 'app.template.template_dir' ]
+        $file = $this->app->getWebTemplatePath() . $this->config[ 'app.template.template' ]
+            . DIRECTORY_SEPARATOR . $this->config[ 'app.template.template_style' ]
             . DIRECTORY_SEPARATOR . $view
-            . $this->container->config[ 'app.template.suffix' ];
+            . $this->config[ 'app.template.suffix' ];
         return $file;
     }
 
--- src/Template.php
+++ src/Template.php
@@ -39,30 +39,59 @@ class Template
     private $time;
 
     /**
+     * 缓存文件是否需要检查过期
+     * auto_update=true  的时候需要检查
+     * auto_update=false 的时候不需要检查
+     * @var bool
+     */
+    private $cacheFileCheckStatus = false;
+
+    /**
      * 构造方法
      * @return void
      */
     public function __construct( ConfigInterface $config )
     {
         $this->config = $config;
+        $template = $config[ 'web_template_path' ] . $config[ 'app.template.template' ] . DIRECTORY_SEPARATOR; //设置系统模板文件的存放目录
+        $template_style = $config[ 'app.template.template_style' ];
         $this->_options = array(
-            'template_dir' => $config[ 'web_template_path' ] . $config[ 'app.template.template' ], //设置系统模板文件的存放目录
-            'cache_dir' => $config[ 'var_path' ] . $config[ 'app.template.cache_dir' ] . DIRECTORY_SEPARATOR . APP_NAME . DIRECTORY_SEPARATOR, //指定缓存文件存放目录
+            'template' => $template,
+            'template_style' => $template_style,//设置模板风格目录名
+            'template_dir' => $template . $template_style . DIRECTORY_SEPARATOR, //设置系统模板文件的存放目录
+            'cache_dir' => $config[ 'var_path' ] . $config[ 'app.template.cache_dir' ] . DIRECTORY_SEPARATOR . APP_NAME . DIRECTORY_SEPARATOR . $template_style . DIRECTORY_SEPARATOR, //指定缓存文件存放目录
+            /**
+             * 当模板文件有改动时重新生成缓存 关闭该项会快一些 开发环境打开、生产环境建议关闭 上线模板后删除一下缓存
+             * 生产环境打开的情况下，会调用check方法检查模板过期
+             * 
+             * true:  模板缓存文件中的check方法会调用 如果模板文件有变动会重新生成模板缓存文件
+             * 
+             * false: 模板缓存文件中的check方法不会调用 每次加载模板缓存时会跳过模板文件的变动检查，性能更高。
+             *        适合在生产环境，此设置后模板变动，上线后清空一下模板缓存
+             */
             'auto_update' => true, //当模板文件改动时是否重新生成缓存
             'cache_lifetime' => 0, //缓存生命周期(分钟)，为 0 表示永久
             'suffix' => '.html', //模板文件后缀
+            /**
+             * true: 加载模板时，会判断模板缓存文件是否存在。
+             *                    不存在就生存模板缓存文件
+             *                    存在就加载模板缓存文件（再根据auto_update状态是否需要check检查模板是否有更新）
+             *
+             * false: 加载模板时，会跳过判断模板缓存文件存在检查。每次都重新生成模板缓存文件
+             */
             'cache_open' => true, //是否开启缓存，程序调试时使用
             'value' => array()
         );
         $this->setTime( $config[ 'now_time' ] );
+        $this->setCacheFileCheckStatus();
     }
 
-    public function getTime()
+    private function getTime()
     {
         return $this->time;
     }
 
-    public function setTime( $time )
+    private function setTime( $time )
     {
         $this->time = $time;
     }
@@ -87,20 +116,27 @@ class Template
     public function set( $name, $value )
     {
         switch ( $name ) {
+            case 'template':
+                $this->_options[ 'template' ] = $value;
+                break;
+            case 'template_style':
+                $this->_options[ 'template_style' ] = $value;
+                break;
             case 'template_dir':
-                $value = $this->_trimpath( $value );
+                $value = $this->trimPath( $value );
                 if ( !is_dir( $value ) )
                     throw new TmacException ( "未找到指定的模板目录 \"$value\"" );
                 $this->_options[ 'template_dir' ] = $value;
                 break;
             case 'cache_dir':
                 if ( !is_dir( $value ) ) {
-                    $makepath = $this->_makepath( $value );
+                    $makepath = $this->makePath( $value );
                 }
                 $this->_options[ 'cache_dir' ] = $value;
                 break;
             case 'auto_update':
                 $this->_options[ 'auto_update' ] = ( boolean ) $value;
+                $this->setCacheFileCheckStatus();
                 break;
             case 'cache_lifetime':
                 $this->_options[ 'cache_lifetime' ] = ( float ) $value;
@@ -120,6 +156,25 @@ class Template
     }
 
     /**
+     * 缓存文件是否需要检查过期
+     * @return bool 0：每次检查 1：不检查过期
+     */
+    private function getCacheFileCheckStatus()
+    {
+        return $this->cacheFileCheckStatus;
+    }
+
+    /**
+     * 缓存文件是否需要检查过期
+     * auto_update=true  的时候需要检查
+     * auto_update=false 的时候不需要检查
+     */
+    private function setCacheFileCheckStatus()
+    {
+        $this->cacheFileCheckStatus = $this->_options[ 'auto_update' ] ? 0 : 1;
+    }
+
+    /**
      * 通过魔术方法设定模板参数
      * @param string $name 参数名称
      * @param mixed $value 参数值
@@ -135,7 +190,7 @@ class Template
      * 缓存是否开启
      * @return boolean
      */
-    public function isCacheOpened()
+    private function isCacheOpened()
     {
         return $this->_options[ 'cache_open' ];
     }
@@ -145,10 +200,10 @@ class Template
      * @param string $file 模板文件名称
      * @return string
      */
-    public function getFile( $file )
+    private function getFile( $file )
     {
         $file = $file . $this->_options[ 'suffix' ];   //设置模板文件后缀
-        $cachefile = $this->_getCacheFile( $file );
+        $cachefile = $this->getCacheFile( $file );
         if ( $this->isCacheOpened() ) {
             if ( !is_file( $cachefile ) )
                 $this->cache( $file );
@@ -181,9 +236,9 @@ class Template
      * @param type $expireTime 生成模板时间
      * @return type
      */
-    public function check( $file, $fname, $expireTime )
+    private function check( $file, $fname, $expireTime )
     {
-        //如果开启当模板文件有改动时重新生成缓存 且 模板文件的最后修改时间大于模板缓存生成时间            
+        //如果开启当模板文件有改动时重新生成缓存 且 模板文件的最后修改时间大于模板缓存生成时间
         if ( $this->_options[ 'auto_update' ] && filemtime( $fname ) > $expireTime ) {
             $this->cache( $file );
             return true;
@@ -200,9 +255,9 @@ class Template
      * @param string $file 模板文件名称
      * @return void
      */
-    public function cache( $file )
+    private function cache( $file )
     {
-        $tplfile = $this->_getTplFile( $file );
+        $tplfile = $this->getTplFile( $file );
 
         if ( !is_readable( $tplfile ) ) {
             throw new TmacException ( "模板文件 \"$tplfile\" 未找到或者无法打开" );
@@ -213,14 +268,14 @@ class Template
         //开始执行把模板中的include文件读进来
         $this->templates = array();
         for ( $i = 1; $i <= 3; $i++ ) {//最多支持3级嵌套,模板中嵌套层级太多也没有啥意义了
-            if ( $this->strexists( $template, '{template_common' ) ) {
+            if ( $this->strExists( $template, '{template_common' ) ) {
                 $template = preg_replace_callback( "/[\n\r\t]*(\<\!\-\-)?\{template_common\s+([a-z0-9_:\/]+)\}(\-\-\>)?[\n\r\t]*/is", function ( $matches ) {
                     return $this->loadTemplateCommon( $matches[ 2 ] );
                 }, $template );
             }
-            if ( $this->strexists( $template, '{template' ) ) {
+            if ( $this->strExists( $template, '{template' ) ) {
                 $template = preg_replace_callback( "/[\n\r\t]*(\<\!\-\-)?\{template\s+([a-z0-9_:\/]+)\}(\-\-\>)?[\n\r\t]*/is", function ( $matches ) {
-                    return $this->loadtemplate( $matches[ 2 ] );
+                    return $this->loadTemplate( $matches[ 2 ] );
                 }, $template );
             }
         }
@@ -245,52 +300,52 @@ class Template
         //增加了\-\>用来在模板中支持$article->title对象的输出        
         $template = preg_replace( "/\{(\\\$[a-zA-Z0-9_\[\]\'\"\$\.\x7f-\xff\-\>]+)\}/s", "<?php echo \\1;?>", $template );
         $template = preg_replace_callback( "/$varRegexp/s", function ( $matches ) {
-            return $this->addquote( '<?php echo ' . $matches[ 1 ] . ';?>' );
+            return $this->addQuote( '<?php echo ' . $matches[ 1 ] . ';?>' );
         }, $template );
         $template = preg_replace_callback( "/\<\?php echo \<\?php echo $varRegexp;\?\>;\?\>/s", function ( $matches ) {
-            return $this->addquote( '<?php echo ' . $matches[ 1 ] . ';?>' );
+            return $this->addQuote( '<?php echo ' . $matches[ 1 ] . ';?>' );
         }, $template );
 
         //替换前台模板载入命令|edit 通用模板替换{tpl 'admin/index'}
         $template = preg_replace( "/[\n\r\t]*\{tpl\s+(.+?)\}[\n\r\t]*/is", "<?php self::show('{$this->config[ 'app.template.template_dir' ]}/\\1'); ?>", $template );
         //替换特定函数
         $template = preg_replace_callback( "/[\n\r\t]*\{eval\s+(.+?)\}[\n\r\t]*/is", function ( $matches ) {
-            return $this->stripvtags( '<?php ' . $matches[ 1 ] . ' ?>', '' );
+            return $this->stripTags( '<?php ' . $matches[ 1 ] . ' ?>', '' );
         }, $template );
         /* 替换${} <?php ?> */
         $template = preg_replace_callback( "/[\n\r\t]*\\\${(.+?)\}[\n\r\t]*/is", function ( $matches ) {
-            return $this->stripvtags( '<?php ' . $matches[ 1 ] . ' ?>', '' );
+            return $this->stripTags( '<?php ' . $matches[ 1 ] . ' ?>', '' );
         }, $template );
         $template = preg_replace_callback( "/[\n\r\t]*\{echo\s+(.+?)\}[\n\r\t]*/is", function ( $matches ) {
-            return $this->stripvtags( '<?php echo ' . $matches[ 1 ] . '; ?>', '' );
+            return $this->stripTags( '<?php echo ' . $matches[ 1 ] . '; ?>', '' );
         }, $template );
         $template = preg_replace_callback( "/([\n\r\t]*)\{elseif\s+(.+?)\}([\n\r\t]*)/is", function ( $matches ) {
-            return $this->stripvtags( $matches[ 1 ] . '<?php } elseif(' . $matches[ 2 ] . ') { ?>' . $matches[ 3 ] . '', '' );
+            return $this->stripTags( $matches[ 1 ] . '<?php } elseif(' . $matches[ 2 ] . ') { ?>' . $matches[ 3 ] . '', '' );
         }, $template );
         $template = preg_replace( "/([\n\r\t]*)\{else\}([\n\r\t]*)/is", "\\1<?php } else { ?>\\2", $template );
 
 
         //替换循环函数及条件判断语句
         $template = preg_replace_callback( "/\{if\s+(.+?)\}/is", function ( $matches ) {
-            return $this->stripvtags( '<?php if(' . $matches[ 1 ] . ') { ?>', '' );
+            return $this->stripTags( '<?php if(' . $matches[ 1 ] . ') { ?>', '' );
         }, $template );
         $template = preg_replace_callback( "/\{else\}/is", function () {
-            return $this->stripvtags( '<?php } else { ?>', '' );
+            return $this->stripTags( '<?php } else { ?>', '' );
         }, $template );
         $template = preg_replace_callback( "/\{elseif\s+(.+?)\}/is", function ( $matches ) {
-            return $this->stripvtags( '<?php } elseif (' . $matches[ 1 ] . ') { ?>', '' );
+            return $this->stripTags( '<?php } elseif (' . $matches[ 1 ] . ') { ?>', '' );
         }, $template );
         $template = preg_replace_callback( "/\{\/if\}/is", function () {
-            return $this->stripvtags( '<?php } ?>', '' );
+            return $this->stripTags( '<?php } ?>', '' );
         }, $template );
         $template = preg_replace_callback( "/\{loop\s+\<\?php echo (\S+);\?\>\s+\<\?php echo (\S+);\?\>\}/is", function ( $matches ) {
-            return $this->stripvtags( '<?php if(is_array(' . $matches[ 1 ] . ')) foreach(' . $matches[ 1 ] . ' AS ' . $matches[ 2 ] . ') { ?>', '' );
+            return $this->stripTags( '<?php if(is_array(' . $matches[ 1 ] . ')) foreach(' . $matches[ 1 ] . ' AS ' . $matches[ 2 ] . ') { ?>', '' );
         }, $template );
         $template = preg_replace_callback( "/\{loop\s+\<\?php echo (\S+);\?\>\s+\<\?php echo (\S+);\?\>\s+\<\?php echo (\S+);\?\>\}/is", function ( $matches ) {
-            return $this->stripvtags( '<?php if(is_array(' . $matches[ 1 ] . ')) foreach(' . $matches[ 1 ] . ' AS ' . $matches[ 2 ] . ' => ' . $matches[ 3 ] . ') { ?>', '' );
+            return $this->stripTags( '<?php if(is_array(' . $matches[ 1 ] . ')) foreach(' . $matches[ 1 ] . ' AS ' . $matches[ 2 ] . ' => ' . $matches[ 3 ] . ') { ?>', '' );
         }, $template );
         $template = preg_replace_callback( "/\{\/loop\}/is", function () {
-            return $this->stripvtags( '<?php } ?>', '' );
+            return $this->stripTags( '<?php } ?>', '' );
         }, $template );
 
         //常量替换
@@ -301,18 +356,19 @@ class Template
 
         //其他替换
         $template = preg_replace_callback( "/\"(http)?[\w\.\/:]+\?[^\"]+?&[^\"]+?\"/", function ( $matches ) {
-            return $this->transamp( $matches[ 0 ] );
+            return $this->transAmp( $matches[ 0 ] );
         }, $template );
         $template = preg_replace_callback( "/\<script[^\>]*?src=\"(.+?)\".*?\>\s*\<\/script\>/is", function ( $matches ) {
-            return $this->stripscriptamp( $matches[ 1 ] );
+            return $this->stripScriptAmp( $matches[ 1 ] );
         }, $template );
         $template = preg_replace_callback( "/[\n\r\t]*\{block\s+([a-zA-Z0-9_]+)\}(.+?)\{\/block\}/is", function ( $matches ) {
-            return $this->stripblock( $matches[ 1 ], $matches[ 2 ] );
+            return $this->stripBlock( $matches[ 1 ], $matches[ 2 ] );
         }, $template );
 
 
-        $auto_update = $this->_options[ 'auto_update' ] ? 0 : 1;
-        $headeradd = "\n{$auto_update}\n";
+        $headeradd = "\n";
+        $headeradd .= '$this->getCacheFileCheckStatus()';
+        $headeradd .= "\n";
         $headeradd .= '|| $this->check(' . "'$file', '$tplfile', " . $this->getTime() . ")\n";
         if ( !empty ( $this->templates ) ) {
             foreach ( $this->templates as $fname ) {
@@ -323,8 +379,8 @@ class Template
         $template = "<?php {$headeradd}?>\r\n$template";
 
         //写入缓存文件
-        $cachefile = $this->_getCacheFile( $file );
-        $makepath = $this->_makepath( $cachefile );
+        $cachefile = $this->getCacheFile( $file );
+        $makepath = $this->makePath( $cachefile );
         if ( $makepath !== true )
             throw new TmacException ( "无法创建缓存目录 \"$makepath\"" );
         file_put_contents( $cachefile, $template );
@@ -335,7 +391,7 @@ class Template
      * @param string $path 路径名称
      * @return string
      */
-    protected function _trimpath( $path )
+    private function trimPath( $path )
     {
         return str_replace( array( '/', '\\', '//', '\\\\' ), self::DIR_SEP, $path );
     }
@@ -345,9 +401,9 @@ class Template
      * @param string $file 模板文件名称
      * @return string
      */
-    protected function _getTplFile( $file )
+    private function getTplFile( $file )
     {
-        return $this->_trimpath( $this->_options[ 'template_dir' ] . self::DIR_SEP . $file );
+        return $this->trimPath( $this->_options[ 'template_dir' ] . self::DIR_SEP . $file );
     }
 
     /**
@@ -355,10 +411,11 @@ class Template
      * @param string $file 模板文件名称
      * @return string
      */
-    protected function _getCacheFile( $file )
+    private function getCacheFile( $file )
     {
-        $file = preg_replace( '/\.[a-z0-9\-_]+$/i', '.cache.php', $file );
-        return $this->_trimpath( $this->_options[ 'cache_dir' ] . self::DIR_SEP . $file );
+        //$file = preg_replace( '/\.[a-z0-9\-_]+$/i', '.cache.php', $file );
+        $file = str_replace( $this->_options[ 'suffix' ], '.cache.php', $file );
+        return $this->trimPath( $this->_options[ 'cache_dir' ] . self::DIR_SEP . $file );
     }
 
     /**
@@ -366,16 +423,16 @@ class Template
      * @param string $path 路径/文件夹名称
      * @return string
      */
-    protected function _makepath( $path )
+    private function makePath( $path )
     {
-        /*      $dirs = explode(self::DIR_SEP, dirname($this->_trimpath($path)));
+        /*      $dirs = explode(self::DIR_SEP, dirname($this->trimPath($path)));
           $tmp = '';
           foreach ($dirs as $dir) {
           $tmp .= $dir . self::DIR_SEP;
           if (!file_exists($tmp) && !@mkdir($tmp, 0777))
           return $tmp;
           } */
-        $dirs = dirname( $this->_trimpath( $path ) );
+        $dirs = dirname( $this->trimPath( $path ) );
         if ( !is_dir( $dirs ) && !mkdir( $dirs, 0777, true ) ) {
             return $dirs;
         }
@@ -390,7 +447,7 @@ class Template
      * 替换&,&amp;amp;,\" 为正常
      * @param $template 模板文件
      */
-    public function transamp( $template )
+    private function transAmp( $template )
     {
         $template = str_replace( '&', '&amp;', $template );
         $template = str_replace( '&amp;amp;', '&amp;', $template );
@@ -401,25 +458,25 @@ class Template
     /**
      * 替换tag中的反斜线
      */
-    public function stripvtags( $expr, $statement )
+    private function stripTags( $expr, $statement )
     {
         $expr = str_replace( "\\\"", "\"", preg_replace( "/\<\?php echo (\\\$.+?);\?\>/s", "\\1", $expr ) );
         $statement = str_replace( "\\\"", "\"", $statement );
         return $expr . $statement;
     }
 
-    public function addquote( $var )
+    private function addQuote( $var )
     {
         return str_replace( "\\\"", "\"", preg_replace( "/\[([a-zA-Z0-9_\-\.\x7f-\xff]+)\]/s", "['\\1']", $var ) );
     }
 
-    public function stripscriptamp( $s )
+    private function stripScriptAmp( $s )
     {
         $s = str_replace( '&amp;', '&', $s );
         return "<script src=\"$s\" type=\"text/javascript\"></script>";
     }
 
-    public function stripblock( $var, $s )
+    private function stripBlock( $var, $s )
     {
         $s = str_replace( '\\"', '"', $s );
         $s = preg_replace( "/<\?php echo \\\$(.+?);\?>/", "{\$\\1}", $s );
@@ -435,7 +492,7 @@ class Template
         return "<?php \n$constadd\$$var = <<<EOF\n" . $s . "\nEOF;\n?>";
     }
 
-    public function strexists( $string, $find )
+    private function strExists( $string, $find )
     {
         return !( strpos( $string, $find ) === FALSE );
     }
@@ -445,9 +502,9 @@ class Template
      * @param type $file
      * @return type
      */
-    public function loadtemplate( $file )
+    private function loadTemplate( $file )
     {
-        $tplfile = $this->_options[ 'template_dir' ] . DIRECTORY_SEPARATOR . $this->_options[ 'template_dir' ] . DIRECTORY_SEPARATOR . $file . $this->_options[ 'suffix' ];
+        $tplfile = $this->_options[ 'template_dir' ] . $file . $this->_options[ 'suffix' ];
         if ( $content = file_get_contents( $tplfile ) ) {
             $this->templates[] = $tplfile;
             return $content;
@@ -461,9 +518,9 @@ class Template
      * @param type $file
      * @return type
      */
-    public function loadTemplateCommon( $file )
+    private function loadTemplateCommon( $file )
     {
-        $tplfile = $this->_options[ 'template_dir' ] . DIRECTORY_SEPARATOR . 'common' . DIRECTORY_SEPARATOR . $file . $this->_options[ 'suffix' ];
+        $tplfile = $this->_options[ 'template' ] . 'common' . DIRECTORY_SEPARATOR . $file . $this->_options[ 'suffix' ];
         if ( $content = file_get_contents( $tplfile ) ) {
             $this->templates[] = $tplfile;
             return $content;
